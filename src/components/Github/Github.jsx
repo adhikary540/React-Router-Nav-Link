6a74044a9cb4c826401872451594e7d40485ab33
chandan adhikary <adhikary540@gmail.com>
2026-06-17 01:09:17 +0530

    Code update

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -3,15 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 
 function Github(){
     const data = useLoaderData()
-    // const [data, setdata] = useState([])
-    // useEffect(()=>{
-    //     fetch('https://api.github.com/users/adhikary540') 
-    //     .then(Response => Response.json())
-    //     .then(data=>{
-    //         console.log(data);
-    //        setdata(data)
-    //     })
-    // }, [])
+
     return (
         <div className="text-center bg-green-600 text-white py-5 px-5">Github Followers: {data.followers}
         <img src={data.avatar_url} width={300} />
