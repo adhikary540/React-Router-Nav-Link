9f0cc555ab2ecfb1d83aab23c556c820e37d0e86
chandan adhikary <adhikary540@gmail.com>
2026-06-17 10:04:08 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Header from './components/Header/header'
+import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
 import './App.css'
